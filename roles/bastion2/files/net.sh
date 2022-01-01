08468d6eba8237c2dc07baf76336f05a2e11a78a
elajoie <elajoie@ericlajoie.com>
2022-01-01 16:40:06 +0100

    updated to vlan400 and ip

--- roles/bastion2/files/net.sh
+++ roles/bastion2/files/net.sh
@@ -3,4 +3,4 @@ while ! ip ad sh eth0 | grep "state UP" > /dev/null; do
   sleep 1
 done
 
-nmcli con modify Wired\ connection\ 1 ipv4.method manual ipv4.addresses 10.16.0.8/24 ipv4.gateway 10.16.0.1 ipv4.dns 8.8.8.8 ipv6.method disabled autoconnect yes
+nmcli con modify Wired\ connection\ 1 ipv4.method manual ipv4.addresses 172.16.0.8/24 ipv4.gateway 172.16.0.1 ipv4.dns 8.8.8.8 ipv6.method disabled autoconnect yes
